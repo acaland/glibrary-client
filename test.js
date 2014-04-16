3f7bf3dee89b69370f8d7325f907863970ee1a9e
Antonio Calanducci <antonio.calanducci@ct.infn.it>
2014-04-16 18:09:21 +0200

    added uploadAndRegister

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var sync = require('synchronize');
 var glibrary = require('./lib/glibrary');
 
 function main() {
-	var response = glibrary.uploadFileSync({filename: 'test.js', vo: 'vo.dch-rp.eu', relativePath: '/vo.dch-rp.eu/test/'});
-	console.log(response);
+	var response = glibrary.uploadFileSync({filename: 'test.js', vo: 'vo.dch-rp.eu', relativePath: 'test/'});
+	//console.log(response);
 	if (response.status == 'success') {
 		console.log("upload completed");
 		glibrary.createEntrySync({
@@ -27,7 +27,7 @@ function main() {
 		});
 	} else {
 		console.log("some error occurred");
-		console.log(response.body);
+		//console.log(response.body);
 	}
 }
 
@@ -53,5 +53,28 @@ function apiTest() {
 	console.log(response);
 }
 
-sync.fiber(apiTest);
+function uploadAndRegisterTest() {
+	var response = glibrary.uploadAndRegister({
+		filename: 'test.js',
+		repo: 'EEE',
+		type: 'Bin',
+		metadata: {
+				FileName: 'test222.js',
+				Description: 'questa una prova di upload',
+				Keywords: 'pippo,pappo,filippo',
+				SubmissionDate: '2014-04-16 14:55',
+				LastModificationDate: '2013-12-14 10:42',
+				Size: 13245,
+				FileType: 'JS',
+				CaptureLocation: 'Catania',
+				CaptureDate: '2014-12-14 10:42',
+				StartTime: '2014-10-10 10:32',
+				StopTime: '2013-10-04 03:32'
+		}
+	});
+	console.log(response);
+}
+
+
+sync.fiber(main);
 
