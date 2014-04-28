5f5ae04679539f3f7ce34207f5df1a755df1328c
Antonio Calanducci <antonio.calanducci@ct.infn.it>
2014-04-28 20:06:06 +0200

    added new test function

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var sync = require('synchronize');
-
 var glibrary = require('./lib/glibrary');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+sync(fs, 'stat');
+var execSync = sync(exec);
+//sync(child_process, 'exec');
+
 
 function main() {
 	var response = glibrary.uploadFileSync({filename: 'test.js', vo: 'vo.dch-rp.eu', relativePath: 'test/'});
@@ -72,9 +78,65 @@ function uploadAndRegisterTest() {
 				StopTime: '2013-10-04 03:32'
 		}
 	});
-	//console.log("sono qui");
+	console.log("io ho finito adesso");
 	console.log(response);
+};
+
+
+function formatDate(date) {
+	return date.toISOString().substr(0,10) + ' ' + date.toLocaleTimeString();
+};
+
+
+function uploadAndRegisterRawData() {
+	var path = process.argv[2]; //read the path from command line
+	if (!path) {
+		console.log("path is a required parameter");
+		return;
+	}
+	//var results = child_process.exec('ls -1 *.bin'); //, function(err, stdout, stderr) {
+	try {
+		process.chdir(path);
+		console.log("Current directory: " + process.cwd());
+		var stdout = execSync('ls -1 *.bin');
+		//console.log(JSON.stringify(stdout));
+	} catch(err) {
+		console.error(err);
+		return;
+	}
+	var files = stdout.split('\n');
+	files.pop(); // remove the last empty line
+	//console.log(files);
+	for (var i=0; i < files.length; i++) {
+		console.log("Processing: " + files[i]);
+		var mtime = fs.statSync(files[i]).mtime;
+		var metadata = {
+			FileName: files[i],
+			SubmissionDate: formatDate(new Date()),
+			LastModificationDate: formatDate(mtime),
+			Size: fs.statSync(files[i]).size,
+			CaptureLocation: files[i].substr(0,5),
+			CaptureDate: files[i].substr(6,10),
+			StopTime: formatDate(mtime)
+		} 
+		console.log(metadata);
+		try {
+			var response = glibrary.uploadAndRegisterSync({
+			 	filename: files[i],
+			 	repo: 'EEE',
+			 	type: 'Bin',
+			 	metadata: metadata
+			});
+			console.log(response);
+		} catch(e) {
+			console.log(e);
+			console.log("Upload failed for " + files[i]);
+			return;
+		}
+		
+	}
 }
+
 
 glibrary.configure({
         //defaultStorage: 'se.reef.man.poznan.pl',
@@ -85,5 +147,9 @@ glibrary.configure({
         defaultRootPath: '/dpm/ct.infn.it/home/'
 });
 
-sync.fiber(uploadAndRegisterTest);
+
+
+
+
+sync.fiber(uploadAndRegisterRawData);
 
